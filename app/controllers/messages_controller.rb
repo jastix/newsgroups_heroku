1e02c9fa0e92f765deb8e334c89554e52ebca022
Nikolay Petrachkov <nik@jastix.me>
2011-03-07 18:27:58 +0100

    fixing search

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     @messages = if params[:q].blank?
       Message.all.paginate(:per_page => 30, :page => params[:page])
   else
-   poisk = Message.search(:include => [:address, :category, :subject, :address => :organization])do |s|
+   poisk = Message.search(:include => [:address, :category, :subject]) do |s|
       s.keywords params[:q], :highlight => true, :merge_continuous_fragments => true, :phrase_fields => {:title => 2.0, :from => 1.5}
     end
     poisk.results
